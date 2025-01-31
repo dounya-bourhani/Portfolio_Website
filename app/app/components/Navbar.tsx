4668b19af1f31bdee96495dc58dae33bd1fdf817
dounya-bourhani <bourhani.dounya@gmail.com>
2025-01-31 18:09:37 +0100

    config

---
+++ app/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-primary">
+      <a className="btn btn-ghost text-xl">Dounya Bourhani</a>
+      <a className="btn btn-ghost">About me</a>
+      <a className="btn btn-ghost">Projects</a>
+      <a className="btn btn-ghost">SKills</a>
+      <a className="btn btn-ghost">Leave a message!</a>
+    </div>
+  )
+}
+
+export default Navbar
