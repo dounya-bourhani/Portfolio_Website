6ab24f2014335e52ec39d67737568aec2a1fa35f
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-01 10:51:15 +0100

    bold important info

--- app/app/components/Presentation.tsx
+++ app/app/components/Presentation.tsx
@@ -7,7 +7,7 @@ const Presentation = () => {
   return (
     <section id='home' className='prose prose-lg !max-w-none flex flex-col items-center justify-center p-10 border shadow-2xl rounded-lg my-4'>
       <h1 >Hi, I'm {' '}
-        <span className="text-primary text-extrabold">
+        <span className="text-primary">
           <Typewriter
             words={['Dounya :)', 'a Data Analyst']}
             loop={0} // Infinite loop
@@ -23,18 +23,18 @@ const Presentation = () => {
 
         <div>
           <p className='text-center text-base'>
-            Hey! I'm Dounya, a recent graduate from the University of Lyon 2, who's incredibly excited in building her own path in the Data World.
-            While obtaining my SISE master degree (Statistics and Computer science applied to Data Science), I worked on a number of projects that 
-            allowed me to shape my skills around data analysis.
+            Hey! I'm Dounya, a recent graduate from the <span className='font-black text-fuchsia-900'>University of Lyon 2</span>, who's incredibly excited in 
+            building her own path in the Data World. While obtaining my <span className='font-black text-fuchsia-900'>SISE master degree</span> (Statistics and 
+            Computer science applied to Data Science), I worked on a number of projects that allowed me to shape my skills around data analysis.
           </p>
           {/* <p className='text-center text-base'>
             I'm currently looking for a position as a Data Analyst, where I can leverage my skills in data analysis, data visualization, and 
             machine learning to help companies make data-driven decisions.
           </p> */}
           <p className='text-center text-base'>
-            Outside of career and academics, I enjoy playing basketball, as I am also a professionnal basketball player. I also enjoy watching 
-            movies/series (never binge-watching, I swear), spending time with families and friends and I'm a huge R&B lover (you'll probably
-            catch me vibing to old school Beyoncé).
+            Outside of career and academics, I enjoy playing basketball, as I am also a <span className='font-black text-fuchsia-900'>professionnal basketball
+            player</span>. I also enjoy watching movies/series (never binge-watching, I swear), spending time with families and friends and I'm a
+            huge fan of R&B and Caribbean music.
           </p>
         </div>
     </section>
