88a6c642d08ffa91ab06686fb258d4c1896c87e2
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-01 02:20:05 +0100

    clean page.tsx + add to about me

--- app/app/components/Presentation.tsx
+++ app/app/components/Presentation.tsx
@@ -20,11 +20,21 @@ const Presentation = () => {
         <div>
           <Image src='/dounya.jpeg' alt='headhsot' width={300} height={300} className='rounded-full border shadow-2xl'/>
         </div>
+
         <div>
           <p className='text-center text-base'>
             Hey! I'm Dounya, a recent graduate from the University of Lyon 2, who's incredibly excited in building her own path in the Data World.
             While obtaining my SISE master degree (Statistics and Computer science applied to Data Science), I worked on a number of projects that 
             allowed me to shape my skills around data analysis.
+          </p>
+          {/* <p className='text-center text-base'>
+            I'm currently looking for a position as a Data Analyst, where I can leverage my skills in data analysis, data visualization, and 
+            machine learning to help companies make data-driven decisions.
+          </p> */}
+          <p className='text-center text-base'>
+            Outside of career and academics, I enjoy playing basketball, as I am also a professionnal basketball player. I also enjoy watching 
+            movies/series (never binge-watching, I swear), spending time with families and friends and I'm a huge R&B lover (you'll probably
+            catch me vibing to old school Beyoncé).
           </p>
         </div>
     </section>
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
 import Navbar from "./components/Navbar";
 import Presentation from "./components/Presentation";
 
