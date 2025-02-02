e8a1155eee71a2fe7fe4504c4df509814260b77a
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-02 15:09:48 +0100

    hovering effects duration

--- app/app/components/Navbar.tsx
+++ app/app/components/Navbar.tsx
@@ -8,10 +8,10 @@ const Navbar = () => {
         <a className='text-xl font-black' href="#home">Dounya Bourhani</a>
       </div>
       <div className='flex-1 items-center justify-end gap-10 text-sm'>
-      <a href='#home' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>About me</a>
-      <a href='#projects' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Projects</a>
-      <a href='#skills' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Skills</a>
-      <a href='#contact' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Contact me!</a>
+      <a href='#home' className='font-black duration-300 hover:text-white hover:underline hover:underline-offset-8 decoration-4'>About me</a>
+      <a href='#projects' className='font-black duration-300 hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Projects</a>
+      <a href='#skills' className='font-black duration-300 hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Skills</a>
+      <a href='#contact' className='font-black duration-300 hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Contact me!</a>
       </div>
     </nav>
 
