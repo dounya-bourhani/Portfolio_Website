87e74e3abfc9eb1d6742bd85c9bc58317a9393f4
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-02 23:06:12 +0100

    fix: carousel sizes and movement

--- app/app/components/Projects.tsx
+++ app/app/components/Projects.tsx
@@ -9,192 +9,50 @@ const Projects = () => {
 
         <div className="carousel carousel-center rounded-box max-w-screen-2xl gap-4">
 
-            <div className="carousel-item card bg-base-100 shadow-xl w-1/5 h-1/2 rounded-lg">
+            <div className="carousel-item card bg-base-100 rounded-lg shadow-xl w-96 h-96 flex flex-col">
                 <figure className="m-0">
                     <img
                     src="./nba_dashboard.png"
                     alt="Tableau Dashboard - Gobert stats"
-                    className="w-96" />
+                    className="w-80" />
                 </figure>
-                <div className="card-body items-center text-center text-xs bg-slate-300">
+                <div className="card-body flex flex-col justify-between items-center text-center text-xs ">
                     <h5 className="card-title">NBA statistics analysis</h5>
-                    <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                        once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
+                    <p>Based on NBA player Rudy Gobert's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
                     <div className="card-actions">
-                        <div className="badge badge-outline">Excel</div>
-                        <div className="badge badge-outline">Python</div>
-                        <div className="badge badge-outline">Tableau</div>
+                        <div className="badge badge-outline text-xs">Excel</div>
+                        <div className="badge badge-outline text-xs">Python</div>
+                        <div className="badge badge-outline text-xs">Tableau</div>
                     </div>
                 </div>
             </div>
 
-            <div className="carousel-item card bg-base-100 shadow-xl w-1/5 h-1/2 rounded-lg">
-                <figure className="m-0">
-                    <img
-                    src="./nba_dashboard.png"
-                    alt="Tableau Dashboard - Gobert stats"
-                    className="w-96" />
-                </figure>
-                <div className="card-body items-center text-center text-xs bg-slate-300">
-                    <h5 className="card-title">NBA statistics analysis</h5>
-                    <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                        once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-                    <div className="card-actions">
-                        <div className="badge badge-outline">Excel</div>
-                        <div className="badge badge-outline">Python</div>
-                        <div className="badge badge-outline">Tableau</div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="carousel-item card bg-base-100 shadow-xl w-1/5 h-1/2 rounded-lg">
-                <figure className="m-0">
-                    <img
-                    src="./nba_dashboard.png"
-                    alt="Tableau Dashboard - Gobert stats"
-                    className="w-96" />
-                </figure>
-                <div className="card-body items-center text-center text-xs bg-slate-300">
-                    <h5 className="card-title">NBA statistics analysis</h5>
-                    <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                        once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-                    <div className="card-actions">
-                        <div className="badge badge-outline">Excel</div>
-                        <div className="badge badge-outline">Python</div>
-                        <div className="badge badge-outline">Tableau</div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="carousel-item card bg-base-100 shadow-xl w-1/5 h-1/2 rounded-lg">
-                <figure className="m-0">
-                    <img
-                    src="./nba_dashboard.png"
-                    alt="Tableau Dashboard - Gobert stats"
-                    className="w-96" />
-                </figure>
-                <div className="card-body items-center text-center text-xs bg-slate-300">
-                    <h5 className="card-title">NBA statistics analysis</h5>
-                    <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                        once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-                    <div className="card-actions">
-                        <div className="badge badge-outline">Excel</div>
-                        <div className="badge badge-outline">Python</div>
-                        <div className="badge badge-outline">Tableau</div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="carousel-item card bg-base-100 shadow-xl w-1/5 h-1/2 rounded-lg">
-                <figure className="m-0">
-                    <img
-                    src="./nba_dashboard.png"
-                    alt="Tableau Dashboard - Gobert stats"
-                    className="w-96" />
-                </figure>
-                <div className="card-body items-center text-center text-xs bg-slate-300">
-                    <h5 className="card-title">NBA statistics analysis</h5>
-                    <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                        once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-                    <div className="card-actions">
-                        <div className="badge badge-outline">Excel</div>
-                        <div className="badge badge-outline">Python</div>
-                        <div className="badge badge-outline">Tableau</div>
-                    </div>
-                </div>
-            </div>
-
-                {/* <div className="carousel-item card bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"
-                        className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center text-sm">
-                        <h4 className="card-title">NBA statsistic analysis</h4>
-                        <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                            once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-                        <div className="card-actions">
-                            <div className="badge badge-outline">Excel</div>
-                            <div className="badge badge-outline">Python</div>
-                            <div className="badge badge-outline">Tableau</div>
-                        </div>
-                    </div>
-                </div> */}
-
-            {/* <div className='carousel-item card bg-base-100 shadow-xl'>
-                    <figure className="px-10 pt-10">
-                        <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"
-                        className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-            </div>
-            <div className='carousel-item card bg-base-100 shadow-xl'>
-
-                    <figure className="px-10 pt-10">
-                        <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes"
-                        className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
-                        </div>
-                    </div>
-            </div> */}
         </div>
         
-
         <h3>Data Science</h3>
-        <div className="carousel carousel-center rounded-box max-w-screen-2xl space-x-4 p-4">
-            <div className="carousel-item w-1/5 rounded-lg bg-slate-300">
-                {/* <img
-                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                className="rounded-box" /> */}
-                <p className='w-1/2'>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
+        
+        <div className="carousel carousel-center rounded-box max-w-screen-2xl gap-4">
+
+            <div className="carousel-item card bg-base-100 shadow-xl w-96 h-96 flex flex-col rounded-lg">
+                <figure className="m-0">
+                    <img
+                    src="./sardeimmo.png"
+                    alt="Tableau Dashboard - Gobert stats"
+                    className="w-80" />
+                </figure>
+                <div className="card-body flex flex-col justify-between items-center text-center text-xs ">
+                    <h5 className="card-title">Sarde Immo</h5>
+                    <p>Develop an application to better understand the market and estimate the sale price of new real estate.</p>
+                    <div className="card-actions">
+                        <div className="badge badge-outline text-xs">Excel</div>
+                        <div className="badge badge-outline text-xs">Python</div>
+                        <div className="badge badge-outline text-xs">Tableau</div>
+                    </div>
+                </div>
             </div>
-            <div className="carousel-item w-1/5 rounded-lg bg-slate-300 rounded-box">
-                {/* <img
-                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                className="rounded-box" /> */}
-                <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-            </div>
-            <div className="carousel-item w-1/5 rounded-lg bg-slate-300">
-                {/* <img
-                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                className="rounded-box" /> */}
-                <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-            </div>
-            <div className="carousel-item w-1/5 rounded-lg bg-slate-300">
-                {/* <img
-                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                className="rounded-box" /> */}
-                <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-            </div>
-            <div className="carousel-item w-1/5 rounded-lg bg-slate-300">
-                {/* <img
-                src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-                className="rounded-box" /> */}
-                <p>Focusing on Minesota's pivot, Rudy Gobert, the goal was to show whether he is going to be able to get the DPOY award, 
-                once again. Based on his game's statistics, I was able to visualize if he's doing a good season so far, or not.</p>
-            </div>
+
         </div>
+
 
     </section>
 
