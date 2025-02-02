19e29b2c7beec5b6cb965d83907079e1ebe67122
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-02 14:35:55 +0100

    hover effects on navbar

--- app/app/components/Navbar.tsx
+++ app/app/components/Navbar.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Navbar = () => {
   return (
 
-    <nav className="navbar bg-primary p-8 rounded-box sticky top-0">
+    <nav className="navbar bg-primary p-8 rounded-box sticky top-0 opacity-75">
       <div>
-        <a className='text-xl' href="#home">Dounya Bourhani</a>
+        <a className='text-xl font-black' href="#home">Dounya Bourhani</a>
       </div>
       <div className='flex-1 items-center justify-end gap-10 text-sm'>
-      <a href='#home'>About me</a>
-      <a href='#projects'>Projects</a>
-      <a href='#skills'>Skills</a>
-      <a href='#contact'>Contact me!</a>
+      <a href='#home' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>About me</a>
+      <a href='#projects' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Projects</a>
+      <a href='#skills' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Skills</a>
+      <a href='#contact' className='font-black hover:text-white hover:underline hover:underline-offset-8 decoration-4'>Contact me!</a>
       </div>
     </nav>
 
