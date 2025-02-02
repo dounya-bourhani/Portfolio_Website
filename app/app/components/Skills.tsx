c3d4e17400385f355d54a1c18e7ec6c061055d3f
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-02 12:09:08 +0100

    initial skills section

---
+++ app/app/components/Skills.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section id='skills' className='prose prose-lg !max-w-none flex flex-col items-center justify-center p-10 border shadow-2xl rounded-lg my-4'>
+        <h2>Skills</h2>
+    </section>
+  )
+}
+
+export default Skills
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 // export const metadata = {
 //   stylesheet: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
@@ -12,6 +13,7 @@ export default function Home() {
       <Navbar/>
       <AboutMe/>
       <Projects/>
+      <Skills/>
     </main>  
   );
 }
