d78013e740c8f1528de414793fe5b45250440a2a
dounya-bourhani <bourhani.dounya@gmail.com>
2025-02-04 22:13:54 +0100

    update : font to patrick hand :))

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Patrick_Hand } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const patrickHand = Patrick_Hand({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="pastel">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono`}
-      >
+      <body className={patrickHand.className}>
         {children}
       </body>
     </html>
